22928b3ba8b3a7d70ffec1c5e90de0ce445fb8b0
徐飞 <d.xuffy@qq.com>
2018-05-03 10:00:50 +0800

    修改

--- src/service/config.js
+++ src/service/config.js
@@ -7,22 +7,24 @@ export default {
     //   mock: 'https://www.easy-mock.com/mock/5abe0744a2146a37688ec3c1/api'
     // },
     local: {
-      BASE_LYF: 'http://dev.uuser-facade.ucnholding.com:18100',
-      BASE_LYF_signin: 'http://dev.usso-facade.ucnholding.com:18100',
+      BASE_LYF: 'http://dev.uuser-facade.ucnscm.com',
+      BASE_LYF_signin: 'http://dev.usso-facade.ucnscm.com',
       MOCK: 'https://www.easy-mock.com/mock/5abe0744a2146a37688ec3c1/api',
-      BASE_JML: 'http://dev.umeta-facade.ucnholding.com',
+      BASE_JML: 'http://dev.umeta-facade.ucnscm.com',
 
-      USSO_URL: 'http://dev.usso-facade.ucnholding.com:18100', // sso 服务
-      UUSER_URL: 'http://dev.uuser-facade.ucnholding.com:18100', // 用户中心
-      UMETA_URL: 'http://dev.umeta-facade.ucnholding.com:18100', // 元数据
+      USSO_URL: 'http://dev.usso-facade.ucnscm.com', // sso 服务
+      UUSER_URL: 'http://dev.uuser-facade.ucnscm.com', // 用户中心
+      UMETA_URL: 'http://dev.umeta-facade.ucnscm.com', // 元数据
     },
     develop: {
-      USSO_URL: 'http://dev.usso-facade.ucnholding.com:18100', // sso 服务
-      UUSER_URL: 'http://dev.uuser-facade.ucnholding.com:18100', // 用户中心
-      UMETA_URL: 'http://dev.umeta-facade.ucnholding.com:18100', // 元数据
+      USSO_URL: 'http://dev.usso-facade.ucnscm.com', // sso 服务
+      UUSER_URL: 'http://dev.uuser-facade.ucnscm.com', // 用户中心
+      UMETA_URL: 'http://dev.umeta-facade.ucnscm.com', // 元数据
     },
     test: {
-      api: 'http://store.test.tailorx.cn/api'
+      USSO_URL: 'http://test.usso-facade.ucnscm.cn', // sso 服务
+      UUSER_URL: 'http://test.uuser-facade.ucnscm.cn', // 用户中心
+      UMETA_URL: 'http://test.umeta-facade.ucnscm.cn', // 元数据
     },
     production: {
       api: 'https://store.tailorx.cn/api'
