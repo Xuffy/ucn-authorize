6cb331c402c9baeaefe5d868af06b95243f7f61d
徐飞 <d.xuffy@qq.com>
2018-05-28 19:41:54 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{noAuth: !show}">
+    <div class="shade"></div>
     <router-view></router-view>
   </div>
 </template>
@@ -8,14 +9,46 @@
 
   export default {
     name: 'app',
-    components: {
+    components: {},
+    data() {
+      return {
+        show: false
+      }
+    },
+    created() {
+      let query = this.$route.query;
+      if (!query.type || !query.redirect) {
+        return false;
+      } else {
+        this.$sessionStore.set('query', query);
+      }
+      this.show = true;
     }
   }
 </script>
 
 
-<style lang="less">
+<style lang="less" scoped>
+  .shade {
+    position: fixed;
+    z-index: 999999;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    display: none;
+  }
 
-  #app {
+  .noAuth .shade {
+    display: block;
+  }
+
+  .noAuth {
+    /*-webkit-filter: grayscale(100%);
+    -moz-filter: grayscale(100%);
+    -ms-filter: grayscale(100%);
+    -o-filter: grayscale(100%);
+    filter: grayscale(100%);
+    filter: gray;*/
   }
 </style>
