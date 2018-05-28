66988619581ed07bc905f313455e3b32ac6564fa
徐飞 <d.xuffy@qq.com>
2018-05-28 18:15:52 +0800

    修改

--- src/lib/lang.js
+++ src/lib/lang.js
@@ -3,17 +3,6 @@ import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 import en from './en/index'
 import zhCN from './zh-CN/index'
 
-/*const configLang = {
-  en: Object.assign(en, enLocale),
-};
-
-let lang = {};
-
-_.map(configLang, (val, key) => {
-  console.log(val, key)
-})
-
-console.log(en, '------');*/
 export default {
   en: Object.assign(en, enLocale),
   zhCN: Object.assign(zhCN, zhLocale)
